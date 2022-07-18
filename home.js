bafe9e4ba1ab9c183ec9418089946a948ccc87c0
irina-marzioni-tdf <104698059+irina-marzioni-tdf@users.noreply.github.com>
2022-07-18 12:57:40 -0300

    initial commit

--- home.js
+++ home.js
@@ -1,21 +1,23 @@
-var botonaso = document.getElementById("cambio");
-var botonaso = document.getElementById("cambio");
-botonaso === null || botonaso === void 0 ? void 0 : botonaso.addEventListener("click", function () {
+window.onload = function () {
+  var botonaso = document.getElementById("cambio");
+
+  botonaso.addEventListener("click", function () {
     var pipipi = Array.from(document.getElementsByClassName("visible"));
     var popopo = Array.from(document.getElementsByClassName("cartelEntrada"));
     if (document.getElementById("menu").classList.contains("visible")) {
-        document.getElementById("menu").classList.remove("visible");
-        document.getElementById("menu").classList.add("ordenado");
+      document.getElementById("menu").classList.remove("visible");
+      document.getElementById("menu").classList.add("ordenado");
     }
     for (var index = 1; index < pipipi.length; index++) {
-        pipipi[index].classList.remove("visible");
+      pipipi[index].classList.remove("visible");
     }
     for (var index = 0; index < popopo.length; index++) {
-        popopo[index].style.display = "none";
+      popopo[index].style.display = "none";
     }
-});
+  });
 
-var rompeBody = document.getElementsById("nover");
-rompeBody.addEventListener("click", function () {
+  var rompeBody = document.getElementsById("nover");
+  rompeBody.addEventListener("click", function () {
     document.getElementById("nover").classList.add("rompeTODO");
-});
+  });
+};
